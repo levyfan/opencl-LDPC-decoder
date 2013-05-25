9c169b48041fb8c8527180ff8faddecb680ca158
FAN Liwen <levyfan@163.com>
2013-05-25 18:20:56 +0800

    how-to build

---
+++ tokenize.m
@@ -0,0 +1,14 @@
+function [ cell_string ] = tokenize( string )
+%TOKENIZE Summary of this function goes here
+%   Detailed explanation goes here
+i = 0;
+while true
+    [token, string] = strtok(string);
+    if isempty(token)
+        break;
+    end
+    i = i+1;
+    cell_string(i) = cellstr(token);
+end
+end
+
